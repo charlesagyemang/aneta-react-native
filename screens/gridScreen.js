55aa482c3e46de93cd1bc8620ff57f14352eaca4
charlesagyemang <micnkru@gmail.com>
2021-01-27 11:57:54 +0000

    feat: Include carosel

--- screens/gridScreen.js
+++ screens/gridScreen.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, ImageBackground } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 import  Button  from "../components/Button";
+import Carousel from 'react-native-snap-carousel';
+
 
 
 export default function Example({navigation}) {
+  const carouselRef = useRef(null);
   const [items, setItems] = React.useState([
     { name: 'All Requests', code: '#8e44ad', url: 'All Requests', uri: 'https://whyy.org/wp-content/uploads/2020/07/2020-7-16-k-paynter-trash-collection-delay-6.jpg' },
     { name: 'Create A New Request', code: '#2c3e50', url: 'New Request', uri: 'https://img.apmcdn.org/205d8706dc865111c47f00e68ab1e51b69535e6d/portrait/6d101a-20190605-trash-carts-st-paul.jpg' },
     { name: 'Track A Pickup', code: '#f1c40f', url: 'New Request', uri: 'https://cityofsugarhill.com/wp-content/uploads/2019/08/waste-pickup-trash-removal.jpg' },
     { name: 'Settings', code: '#e67e22', url: 'Settings', uri: 'https://image.winudf.com/v2/image/Y29tLmdhbWVzY2llbmNlaW5jLnF1aWNrc2V0dGluZ3MuZnJlZV9pY29uXzE1MjYxMzYyNTVfMDM1/icon.png?w=&fakeurl=1' },
   ]);
+  let carousel = ""
+
+
+  const _renderItem = ({item, index}) => {
+      return (
+          <View style={{flex: 1, backgroundColor: item.code}}>
+          <ImageBackground source={{uri: item.uri}} style={styles.image}>
+              <Text style={styles.title}>{ item.name }</Text>
+          </ImageBackground>
+
+          </View>
+      );
+  }
 
   return (
-    <View style={{ flex: 1, marginTop: "60%"}}>
+    <View style={{ flex: 1}}>
+
+      <View style={{ flex: 2, alignItems: 'center' }}>
+        <Carousel
+              ref={(c) => { carousel = c; }}
+              data={items}
+              renderItem={_renderItem}
+              sliderWidth={450}
+              itemWidth={400}
+              layout={'tinder'}
+            />
+        </View>
+
       <FlatGrid itemDimension={130} data={items} style={styles.gridView} spacing={10}
         renderItem={({ item }) => (
           <TouchableOpacity
             style={[styles.itemContainer, { backgroundColor: item.code }]}
             key={item.name}
             onPress={() => navigation.navigate(item.url, { name: item.url })}>
-
             <Text style={styles.itemName}>{item.name}</Text>
             <Text style={styles.itemCode}>{item.code}</Text>
           </TouchableOpacity>
@@ -51,4 +78,32 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#fff',
   },
+  carouselContainer: {
+    height:"30%",
+	},
+	carousel: {
+    flex:1
+	},
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: "60%",
+    marginLeft: "25%",
+    marginRight: "20%",
+    color: 'white'
+
+  },
+
+  image: {
+   flex: 1,
+   resizeMode: "cover",
+   justifyContent: "center"
+ },
+ text: {
+   color: "white",
+   fontSize: 42,
+   fontWeight: "bold",
+   textAlign: "center",
+   backgroundColor: "#000000a0"
+ }
 });
